8b0c62fff0d9c07a657b9dfba472d4d95ca880fd
Fabio Di Nota <fabiodinota@gmail.com>
2024-01-21 00:15:47 +0100

    some initial changes
    cors added to backend
    test api call
    env added
    manifest and icons

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server???");
